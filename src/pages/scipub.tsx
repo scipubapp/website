23c34fdcd3d509c9ec1bcf00f48369de19f59cf8
Sebastian Mellen <s@slm.space>
2022-02-14 15:10:00 -0700

    Headwind

--- src/pages/scipub.tsx
+++ src/pages/scipub.tsx
@@ -8,14 +8,14 @@ export default function SciPub() {
         &#8678; BACK
       </a>
       <h1 className="py-4 mt-10 text-3xl font-bold">SciPub</h1>
-      <p className="py-4 leading-7 text-left">
+      <p className="py-4 text-left leading-7">
         The goals of the SciPub Foundation are so grandiose that they can likely
         never be reached. Their purpose is to serve as a proverbial north star.
         Reliable, unattainable, and If we manage to achieve even 10% of what we
         set out to do, we will change the world. Imagine reaching 10% of the
         speed of light.
       </p>
-      <p className="py-4 leading-7 text-left">
+      <p className="py-4 text-left leading-7">
         Growing access to information and data is perhaps the most persistent
         theme in modern innovation. In the past 25 years, the world has seen a
         radical shift in the availability and accessibility of information and
@@ -25,7 +25,7 @@ export default function SciPub() {
         continuously stream unimaginable volumes of information between
         continents, at near the speed of light.
       </p>
-      <p className="py-4 leading-7 text-left">
+      <p className="py-4 text-left leading-7">
         But science lags far behind other knowledge ecosystems. For a number of
         unfortunate reasons, science has not benefited from the innovation of
         the past 25 years as greatly as other information disciplines have. Vast
@@ -33,7 +33,7 @@ export default function SciPub() {
         a consequence of archaic and exploitative publication models. Why is
         this so?
       </p>
-      <p className="py-4 leading-7 text-left">
+      <p className="py-4 text-left leading-7">
         Scientific journals have an astounding monopoly over today's research
         landscape. Entire fields of science are often dominated by a handful of
         journals, and information transfer within the broader scientific
@@ -42,7 +42,7 @@ export default function SciPub() {
         dominant position, journals are able to extort publishers <em>and</em>{" "}
         readers, in a Kafkaesque (and often taxpayer-funded) arrangement.
       </p>
-      <p className="py-4 leading-7 text-left">
+      <p className="py-4 text-left leading-7">
         By decentralizing publishing technology and providing a legal model for
         decentralized publishing, the SciPub Foundation hopes to change this.
       </p>
