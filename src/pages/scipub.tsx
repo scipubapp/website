ca58760e849eb85b17d64f492bc42eb0330db6f6
Sebastian Mellen <s@slm.space>
2022-03-20 01:49:24 -0400

    write more

--- src/pages/scipub.tsx
+++ src/pages/scipub.tsx
@@ -8,41 +8,50 @@ export default function SciPub() {
         &#8678; BACK
       </a>
       <h1 className="py-4 mt-10 text-3xl font-bold">SciPub</h1>
-      <p className="py-4 text-left leading-7">
+      {/* <p className="py-4 leading-7 text-left">
         The goals of the SciPub Foundation are so grandiose that they can likely
         never be reached. Their purpose is to serve as a proverbial north star.
         Reliable, unattainable, and If we manage to achieve even 10% of what we
         set out to do, we will change the world. Imagine reaching 10% of the
         speed of light.
-      </p>
-      <p className="py-4 text-left leading-7">
+      </p> */}
+      <p className="py-4 leading-7 text-left">
         Growing access to information and data is perhaps the most persistent
-        theme in modern innovation. In the past 25 years, the world has seen a
-        radical shift in the availability and accessibility of information and
-        knowledge. Spurred by the invention of the internet and then the web,
-        people across the globe are able to publish and consume information in a
-        borderless and unbounded manner. Hypertext transfer protocols
-        continuously stream unimaginable volumes of information between
-        continents, at near the speed of light.
+        theme in modern innovation. In the past 25 years, global access to
+        knowledge and information has radically expanded. Spurred by the
+        invention of the internet and the web, people across the globe are able
+        to publish and consume information in a borderless and unbounded manner.
+        Hypertext transfer protocols continuously stream unimaginable volumes of
+        information between continents, at near the speed of light.
       </p>
-      <p className="py-4 text-left leading-7">
-        But science lags far behind other knowledge ecosystems. For a number of
-        unfortunate reasons, science has not benefited from the innovation of
-        the past 25 years as greatly as other information disciplines have. Vast
-        troves of research publications are shielded from the public, largely as
-        a consequence of archaic and exploitative publication models. Why is
-        this so?
+      <p className="py-4 leading-7 text-left">
+        But science lags far behind other information ecosystems. Unfortunately,
+        science has not benefited from the internet-enabled innovation of the
+        past 25 years as greatly as other information ecosystems. Vast troves of
+        research remain shielded from the public, largely as a consequence of
+        archaic and exploitative publication models. Research also remains hard
+        to publish without the approval of sanctioned gatekeepers.
       </p>
-      <p className="py-4 text-left leading-7">
-        Scientific journals have an astounding monopoly over today's research
-        landscape. Entire fields of science are often dominated by a handful of
-        journals, and information transfer within the broader scientific
+      <p className="py-4 leading-7 text-left">
+        The "publishing problem" is threefold, spanning a complex cultural,
+        legal, and technological landscape. Whether because of a dearth of
+        licensing options for openly published research, or inadequate
+        technology for decentralized publishing, scientific publishing is
+        heavily centralized institution. Though the publishing problem is broad
+        and hard to precisely define, one institution can be seen as the source:
+        the scientific journal.
+      </p>
+      <p className="py-4 leading-7 text-left">
+        Scientific journals have a tight monopoly over the means of research
+        publication. Entire fields of science are often dominated by a handful
+        of journals, and information transfer within the broader scientific
         ecosystem rarely occurs outside of "high-impact journals", which are
         highly competitive and expensive to publish in. Because of their
-        dominant position, journals are able to extort publishers <em>and</em>{" "}
-        readers, in a Kafkaesque (and often taxpayer-funded) arrangement.
+        dominant position, journals are able to extract payments from
+        researchers <em>and</em> readers, in a Kafkaesque (and often
+        taxpayer-funded) arrangement.
       </p>
-      <p className="py-4 text-left leading-7">
+      <p className="py-4 leading-7 text-left">
         By decentralizing publishing technology and providing a legal model for
         decentralized publishing, the SciPub Foundation hopes to change this.
       </p>
